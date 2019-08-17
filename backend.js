8f79ae3390ec58bb1ca422d4b19a8d50972e314d
Daniel Mayer <danmayercraigs@gmail.com>
2019-08-17 13:57:24 -0400

    Added variables for the element and form fields, and started and onclick event.

---
+++ backend.js
@@ -0,0 +1,26 @@
+// create vars for the various search  fields with the
+
+// create fumctions for the variablesw
+
+// create button commands
+
+// button on click event to trigger function
+
+// var queryurl with the request site url and key
+
+// response prints to the screen
+
+
+
+var searchDiv  = $("#searchDiv");
+
+var numRecordsToGet = $("#numRecordsToGet");
+
+var submitSearchBtn = $("#submitSearchBtn");
+
+var searchResults = $("#searchResults");
+
+// capture user input in the searchDiv
+$("#submitSearchBtn").on("click" function() {
+    
+}
